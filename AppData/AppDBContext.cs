7779d818d38e8c7519a4b8357a0271089610ea9a
iloveMlord <ilovemilord.op@gmail.com>
2025-10-22 23:44:50 +0700

    Add project files.

---
+++ AppData/AppDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebDevStd2531.Models;
+
+namespace WebDevStd2531.AppData
+{
+    public class AppDBContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<OrderProduct> OrderProducts { get; set; } = null!;
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WebDevStd2531.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public ICollection<OrderProduct>? OrderProducts { get; set; }
+    }
+}
