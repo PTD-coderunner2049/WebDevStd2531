b984bd5815613ea7e6e22fe883f52ca63560e09d
iloveMlord <ilovemilord.op@gmail.com>
2025-11-03 10:00:16 +0700

    cut sidebar to solitary viewcomponent, so it work with other page than just home and dont need copypast or configure their controller to give the same data package to their view. update the hardcode of the detail page span 3 and 6 to display dynamically. span 9 need to wait because the database havent got that complicated yet.

---
+++ Controllers/ProdController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebDevStd2531.AppData;
+using WebDevStd2531.Models;
+
+namespace WebDevStd2531.Controllers
+{
+    public class ProdController : Controller
+    {
+        private readonly AppDBContext _db;
+
+        public ProdController(AppDBContext context)
+        {
+            _db = context;
+        }
+
+        public IActionResult ProdDetail(int Id)
+        {
+            Product? currProd = _db.Products
+                .Include(p => p.Category)
+                .ThenInclude(c => c.GrandCategory)
+                .Where(p => p.Id == Id)     
+                .FirstOrDefault();          
+            return View(currProd);
+            //fully loaded currProd with Category and GrandCategory? for the detail view? ABSOLUTELY UNNECESSARY but why not :D
+        }
+
+    }
+}
---
+++ ViewComponents/CategoryNavViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebDevStd2531.AppData;
+
+public class CategoryNavViewComponent : ViewComponent
+{
+    private readonly AppDBContext _db;
+
+    public CategoryNavViewComponent(AppDBContext context)
+    {
+        _db = context;
+    }
+
+    // This method runs whenever the View Component is called
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var grandCategories = await _db.GrandCategories
+            .Include(gc => gc.Categories)
+            .ThenInclude(c => c.Products) // Still needed for product counts, like always sadly
+            .ToListAsync();
+
+        return View("_Sidebar", grandCategories);
+    }
+}
