1b583be5ec0b70238e95a6a2dba7725fca918ff7
iloveMlord <ilovemilord.op@gmail.com>
2025-11-16 23:52:10 +0700

    create the registerview (empty)

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebDevStd2531.AppData;
+
+namespace WebDevStd2531.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDBContext _db;
+        public UserController(AppDBContext context)
+        {
+            _db = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
